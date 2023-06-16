168eae25303c280c8e336214b7e262fda0e4fdab
Abhishek Byanjankar <2011009_abhishek@kusom.edu.np>
2023-06-16 18:41:53 +0900

    invalid credentials

--- data_class.php
+++ data_class.php
@@ -61,7 +61,7 @@ class data extends db {
         }
 
         else {
-            header("location: index.php?msg=Invalid Credentials");
+            header("location: index.php?msg=Invalid Credentials bot");
         }
 
     }
